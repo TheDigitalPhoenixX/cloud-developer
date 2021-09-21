7b54f277dc537b0b7ac3cc5d9a75c7e9ffd15784
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-09-21 04:36:19 +0200

    feat: url validation

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -16,8 +16,6 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
-  //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
@@ -26,9 +24,28 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
+  app.get("/filteredimage", async (req, res) => {
+    const imageURL: string = req.query.image_url;
 
-  //! END @TODO1
+    if (!imageURL) {
+      return res.status(400).send({ message: "image_url is required " });
+    }
+
+    try {
+      new URL(imageURL);
+    } catch (error) {
+      return res.status(400).send({ message: "image_url is malformed" });
+    }
+
+    if (imageURL.match(/\.(jpg|jpeg|png|bmp|tiff)$/) === null) {
+      return res.status(422).send({
+        message:
+          "image_url is not a URL for an image of type ['jpg', 'jpeg', 'png', 'bmp', 'tiff']",
+      });
+    }
+
+    res.send({ imageURL });
+  });
 
   // Root Endpoint
   // Displays a simple message to the user
